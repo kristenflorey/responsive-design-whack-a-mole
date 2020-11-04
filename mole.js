56ed503d1692a0cbf8679a5340a5f7c61ab8f466
Kristen Florey <kristenmflorey@gmail.com>
2020-11-04 16:24:38 -0500

    moles hoppin

--- mole.js
+++ mole.js
@@ -1,20 +1,35 @@
 window.addEventListener("DOMContentLoaded", (event) => {
     setTimeout(popUpRandomMole, 0);
-
-
+    let molesOne = document.querySelectorAll(".wgs__mole-head")
+    molesOne.forEach((mole) => {
+        mole.addEventListener('click', e => {
+            mole.classList.add('wgs__mole-head--hidden');
+            mole.classList.add('wgs__mole-head--whacked');
+        })
+    })
 });
 
 function popUpRandomMole() {
-    let moles = document.querySelectorAll(".wgs__mole-head");
+    let moles = document.querySelectorAll(".wgs__mole-head:not(.wgs__mole-head--whacked)");
+    if (moles.length === 0) {
+        let winMessage = document.getElementById('.win-message').hidden = false;
+        winMessage.addEventListener('mousemove', e => {
+            e.reload()
+        })
+
+        return;
+    }
     let randomNum = Math.floor(Math.random() * moles.length);
     let randomMole = moles[randomNum];
-    console.log(randomMole.classList)
-    randomMole.classList.remove(".wgs__mole-head--hidden");
-    console.log(randomMole.classList)
-    //setTimeout(hideMole, 1000, randomMole);
+    console.log(moles)
+    randomMole.classList.remove("wgs__mole-head--hidden");
+    // console.log(randomMole.classList)
+    setTimeout(hideMole, 1000, randomMole);
 }
 
 function hideMole(param) {
-    param.classList.add(".wgs__mole-head--hidden");
+    param.classList.add("wgs__mole-head--hidden");
     setTimeout(popUpRandomMole, 1000);
 }
+
+function gameOver(){}
