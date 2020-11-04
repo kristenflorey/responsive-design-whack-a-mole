a80f134019153ddac5a0224688a56a0640592996
Jesse <jbenlind@gmail.com>
2020-11-04 14:29:32 -0600

    phase 2 complete

--- mole.js
+++ mole.js
@@ -1,10 +1,20 @@
-// window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener("DOMContentLoaded", (event) => {
+    setTimeout(popUpRandomMole, 0);
 
-//     setInterval(() => {
-//       const moleHeads = document.querySelectorAll('.wgs__mole-head');
-//       for (let moleHead of moleHeads) {
-//         moleHead.classList.toggle('wgs__mole-head--hidden');
-//       }
-//     }, 1000);
 
-//   });
+});
+
+function popUpRandomMole() {
+    let moles = document.querySelectorAll(".wgs__mole-head");
+    let randomNum = Math.floor(Math.random() * moles.length);
+    let randomMole = moles[randomNum];
+    console.log(randomMole.classList)
+    randomMole.classList.remove(".wgs__mole-head--hidden");
+    console.log(randomMole.classList)
+    //setTimeout(hideMole, 1000, randomMole);
+}
+
+function hideMole(param) {
+    param.classList.add(".wgs__mole-head--hidden");
+    setTimeout(popUpRandomMole, 1000);
+}
